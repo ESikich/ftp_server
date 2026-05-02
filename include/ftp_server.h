906e293237bf1ee4525928b974dae341c3195e39
erik <erik@playground.home.lan>
2026-05-02 01:21:06 -0500

    Revert "Expose returning session API"
    This reverts commit 18161608800600ac2577b33d7a7fe249757036bf.

--- include/ftp_server.h
+++ include/ftp_server.h
@@ -203,14 +203,8 @@ int ftp_data_copy_upload(int dst_fd, int src_fd, int timeout_ms);
 /* ------------------------------------------------------------------ */
 
 /*
- * Serve one FTP session on ctrl_fd using config.
- * Closes ctrl_fd before returning.
- */
-int ftp_session_serve(int ctrl_fd, const ftp_config_t *config);
-
-/*
- * Run one FTP session on ctrl_fd using config, then exit.
- * This is the forked Unix server child entry point.
+ * Run one FTP session on ctrl_fd using config.
+ * Never returns on success (the child process exits).
  */
 _Noreturn void ftp_session_run(int ctrl_fd, const ftp_config_t *config);
 
--- src/ftp_session.c
+++ src/ftp_session.c
@@ -1031,13 +1031,12 @@ dispatch(ftp_session_t *sess, const ftp_cmd_t *cmd,
 /* Session runner                                                       */
 /* ------------------------------------------------------------------ */
 
-int
-ftp_session_serve(int ctrl_fd, const ftp_config_t *config)
+void
+ftp_session_run(int ctrl_fd, const ftp_config_t *config)
 {
     ftp_session_t sess;
     ftp_cmd_t cmd;
     int rc;
-    int status = 0;
 
     memset(&sess, 0, sizeof(sess));
     sess.ctrl_fd    = ctrl_fd;
@@ -1057,15 +1056,12 @@ ftp_session_serve(int ctrl_fd, const ftp_config_t *config)
             if (errno == ETIMEDOUT)
                 ftp_reply_send(ctrl_fd, 421,
                     "Service not available, closing control connection");
-            status = -1;
             break;
         }
 
         rc = dispatch(&sess, &cmd, config);
-        if (rc < 0) {
-            status = -1;
+        if (rc < 0)
             break;
-        }
 
         if (sess.auth == SESSION_CLOSING)
             break;
@@ -1074,12 +1070,5 @@ ftp_session_serve(int ctrl_fd, const ftp_config_t *config)
 done:
     pasv_close(&sess);
     close(ctrl_fd);
-    return status;
-}
-
-void
-ftp_session_run(int ctrl_fd, const ftp_config_t *config)
-{
-    (void)ftp_session_serve(ctrl_fd, config);
     exit(0);
 }
